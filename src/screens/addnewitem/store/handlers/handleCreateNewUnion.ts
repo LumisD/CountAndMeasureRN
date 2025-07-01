2ea347f21f42fa78a282f4b1fc80df83eb7de455
alexSimch <alexey@xealenergy.com>
2025-07-01 14:49:13 -0400

    Create handleCreateNewUnion

---
+++ src/screens/addnewitem/store/handlers/handleCreateNewUnion.ts
@@ -0,0 +1,38 @@
+import {MeasureAndCountRepository} from "../../../../data/repository/MeasureAndCountRepository";
+import {getDefaultUnionTitle} from "../../../utils/generalUtils";
+import {
+  createDefaultUnionOfChipboardsUI,
+  UnionOfChipboardsUI,
+} from "../../../models/UnionOfChipboardsUI";
+import {AddNewItemEffect} from "../../AddNewItemEffect";
+import {AddNewItemState} from "../../AddNewItemState";
+import {AddNewItemStore} from "../AddNewItemStore";
+
+export async function handleCreateNewUnion(
+  repo: MeasureAndCountRepository,
+  get: () => AddNewItemStore,
+): Promise<{newState: AddNewItemState; effect?: AddNewItemEffect}> {
+  const newUnion: UnionOfChipboardsUI = {
+    ...createDefaultUnionOfChipboardsUI(),
+    title: getDefaultUnionTitle(),
+    isFinished: false,
+    createdAt: Date.now(),
+  };
+
+  const unionId = await repo.insertUnionOfChipboards(newUnion);
+
+  const newState: AddNewItemState = {
+    ...get().state,
+    unionOfChipboards: {
+      ...newUnion,
+      id: unionId,
+    },
+    newOrEditChipboard: {
+      ...get().state.newOrEditChipboard,
+      unionId,
+      colorName: "White",
+    },
+  };
+
+  return {newState, effect: undefined};
+}
