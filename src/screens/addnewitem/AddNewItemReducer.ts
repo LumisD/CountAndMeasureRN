95d8f3bd3561af93487abd5b12db5c60d38028fe
alexSimch <alexey@xealenergy.com>
2025-07-01 14:55:01 -0400

    Change addNewItemReducer

--- src/screens/addnewitem/AddNewItemReducer.ts
+++ src/screens/addnewitem/AddNewItemReducer.ts
@@ -1,37 +1,60 @@
-import {AddNewItemEffect} from "./AddNewItemEffect";
-import {AddNewItemIntent} from "./AddNewItemIntent";
+import {
+  AddNewItemEffect,
+  NAVIGATE_BACK,
+  SHOW_DELETE_CONFIRMATION_DIALOG,
+  SHOW_EDIT_CONFIRMATION_DIALOG,
+  SHOW_SHARE_UNION_DIALOG,
+  SHOW_SNACKBAR,
+} from "./AddNewItemEffect";
+import {
+  AddNewItemIntent,
+  ASK_DELETE_CHIPBOARD,
+  ASK_EDIT_CHIPBOARD,
+  PRESS_TO_SHARE_UNION,
+  TOGGLE_ADD_AREA_VISIBILITY,
+} from "./AddNewItemIntent";
 import {AddNewItemState} from "./AddNewItemState";
+
+import {BACK} from "./AddNewItemIntent";
 
 export const addNewItemReducer = (
   state: AddNewItemState,
   intent: AddNewItemIntent,
 ): {newState: AddNewItemState; effect?: AddNewItemEffect} => {
   switch (intent.type) {
-    case "CreateNewUnion":
-      return {
-        newState: state,
-        effect: {type: "CreateNewUnion"},
-      };
-    case "TitleOfUnionChanged":
+    case TOGGLE_ADD_AREA_VISIBILITY:
       return {
         newState: {
           ...state,
-          unionOfChipboards: {
-            ...state.unionOfChipboards,
-            title: intent.newTitle,
-          },
+          isAddAreaOpen: !state.isAddAreaOpen,
         },
-      };
-    case "Back":
-      return {
-        newState: state,
-        effect: {type: "NavigateBack"},
+        effect: undefined,
       };
 
-    case "PressToDeleteUnion":
+    case ASK_EDIT_CHIPBOARD:
       return {
         newState: state,
-        effect: {type: "ShowRemoveUnionDialog"},
+        effect: {
+          type: SHOW_EDIT_CONFIRMATION_DIALOG,
+          chipboard: intent.chipboard,
+          hasColor: state.unionOfChipboards.hasColor,
+        },
+      };
+
+    case ASK_DELETE_CHIPBOARD:
+      return {
+        newState: state,
+        effect: {
+          type: SHOW_DELETE_CONFIRMATION_DIALOG,
+          chipboard: intent.chipboard,
+          hasColor: state.unionOfChipboards.hasColor,
+        },
+      };
+
+    case BACK:
+      return {
+        newState: state,
+        effect: {type: NAVIGATE_BACK},
       };
 
     default:
