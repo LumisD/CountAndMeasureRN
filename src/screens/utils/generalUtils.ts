c1a9dc063bad16289edb1c419248a5bddb19d59b
alexSimch <alexey@xealenergy.com>
2025-07-01 13:53:47 -0400

    Create screen utils

---
+++ src/screens/utils/generalUtils.ts
@@ -0,0 +1,17 @@
+import {t} from "i18next";
+
+export function getCurrentDateTime(): string {
+  const date = new Date();
+  return new Intl.DateTimeFormat("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+  }).format(date);
+}
+
+export function getDefaultUnionTitle(): string {
+  return t("chipboard_sheet_list_title", {value: getCurrentDateTime()});
+}
---
+++ src/screens/utils/shareIntent.ts
@@ -0,0 +1,26 @@
+import {Platform} from "react-native";
+import {PlatformShareIntent} from "../addnewitem/AddNewItemEffect";
+
+export function createPlatformShareIntent(
+  text: string,
+  title: string,
+  t: (key: string) => string,
+): PlatformShareIntent {
+  if (Platform.OS === "android") {
+    return {
+      action: "ACTION_SEND",
+      extras: {
+        EXTRA_TEXT: text,
+        EXTRA_SUBJECT: title,
+      },
+      type: "text/plain",
+      chooserTitle: t("share_chipboards"),
+    };
+  } else {
+    return {
+      message: text,
+      title,
+      subject: title,
+    };
+  }
+}
