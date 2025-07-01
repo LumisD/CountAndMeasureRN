c1d65f29ddadb5b4f6f191fe65fb858e37a690c0
alexSimch <alexey@xealenergy.com>
2025-07-01 14:52:57 -0400

    Change AddNewItemEffect

--- src/screens/addnewitem/AddNewItemEffect.ts
+++ src/screens/addnewitem/AddNewItemEffect.ts
@@ -1,20 +1,48 @@
 import {ChipboardUI} from "./models/ChipboardUI";
 
+export const CREATE_NEW_UNION_EFFECT = "CreateNewUnion";
+export const SHOW_DELETE_CONFIRMATION_DIALOG = "ShowDeleteConfirmationDialog";
+export const SHOW_EDIT_CONFIRMATION_DIALOG = "ShowEditConfirmationDialog";
+export const SHOW_SNACKBAR = "ShowSnackbar";
+export const SHOW_REMOVE_UNION_DIALOG = "ShowRemoveUnionDialog";
+export const SHOW_SHARE_UNION_DIALOG = "ShowShareUnionDialog";
+export const FLASH_ADD_ITEM_AREA = "FlashAddItemArea";
+export const SHARE_UNION = "ShareUnion";
+export const NAVIGATE_BACK = "NavigateBack";
+
+type AndroidShareIntent = {
+  action: "ACTION_SEND";
+  extras: {
+    EXTRA_TEXT: string;
+    EXTRA_SUBJECT: string;
+  };
+  type: "text/plain";
+  chooserTitle: string;
+};
+
+type IOSShareIntent = {
+  message: string;
+  title: string;
+  subject: string;
+};
+
+export type PlatformShareIntent = AndroidShareIntent | IOSShareIntent;
+
 export type AddNewItemEffect =
-  | {type: "CreateNewUnion"}
+  | {type: typeof CREATE_NEW_UNION_EFFECT}
   | {
-      type: "ShowDeleteConfirmationDialog";
+      type: typeof SHOW_DELETE_CONFIRMATION_DIALOG;
       chipboard: ChipboardUI;
       hasColor: boolean;
     }
   | {
-      type: "ShowEditConfirmationDialog";
+      type: typeof SHOW_EDIT_CONFIRMATION_DIALOG;
       chipboard: ChipboardUI;
       hasColor: boolean;
     }
-  | {type: "ShowSnackbar"; message: string}
-  | {type: "ShowRemoveUnionDialog"}
-  | {type: "ShowShareUnionDialog"}
-  | {type: "FlashAddItemArea"}
-  | {type: "ShareUnion"; shareIntent: unknown} // use real type later if needed
-  | {type: "NavigateBack"};
+  | {type: typeof SHOW_SNACKBAR; message: string}
+  | {type: typeof SHOW_REMOVE_UNION_DIALOG}
+  | {type: typeof SHOW_SHARE_UNION_DIALOG}
+  | {type: typeof FLASH_ADD_ITEM_AREA}
+  | {type: typeof SHARE_UNION; shareIntent: PlatformShareIntent}
+  | {type: typeof NAVIGATE_BACK};
