e910dc6a3c3d0efbd39fdc029292cb41fe4ea4d7
alexSimch <alexey@xealenergy.com>
2025-07-01 14:47:16 -0400

    Create handleEditChipboardInAddAreaAndRemoveFromDb

---
+++ src/screens/addnewitem/store/handlers/handleEditChipboardInAddAreaAndRemoveFromDb.ts
@@ -0,0 +1,33 @@
+import {toObjectIdOrUndefined} from "../../../../data/db/utils";
+import {MeasureAndCountRepository} from "../../../../data/repository/MeasureAndCountRepository";
+import {AddNewItemEffect, FLASH_ADD_ITEM_AREA} from "../../AddNewItemEffect";
+import {AddNewItemState} from "../../AddNewItemState";
+import {ChipboardUI} from "../../models/ChipboardUI";
+
+export async function handleEditChipboardInAddAreaAndRemoveFromDb(
+  chipboard: ChipboardUI,
+  repo: MeasureAndCountRepository,
+  get: () => {state: AddNewItemState},
+): Promise<{newState: AddNewItemState; effect?: AddNewItemEffect}> {
+  const currentState = get().state;
+
+  const objectId = toObjectIdOrUndefined(chipboard.id);
+  if (!objectId) {
+    console.error("Invalid chipboard.id, skipping delete");
+    return {newState: currentState, effect: undefined};
+  }
+
+  const newState: AddNewItemState = {
+    ...currentState,
+    isAddAreaOpen: true,
+    isAddButtonAvailable: true,
+    newOrEditChipboard: chipboard,
+  };
+
+  await repo.deleteChipboardById(objectId);
+
+  return {
+    newState,
+    effect: {type: FLASH_ADD_ITEM_AREA},
+  };
+}
