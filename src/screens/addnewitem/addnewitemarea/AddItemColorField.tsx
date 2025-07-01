96fb01a7f9120ed632e8ddd1800f7a3300ee18d3
alexSimch <alexey@xealenergy.com>
2025-07-01 14:26:59 -0400

    Create AddItemColorField

---
+++ src/screens/addnewitem/addnewitemarea/AddItemColorField.tsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import {Text, View, Pressable, StyleSheet} from "react-native";
+import {ColorPickerRow} from "../../common/ColorPickerRow";
+import {colorListWithNames} from "../../common/screenData";
+import {AddNewItemIntent, COLOR_CHANGED} from "../AddNewItemIntent";
+import {ColorItem} from "../../models/ColorItem";
+
+type Props = {
+  colorName: string;
+  processIntent: (intent: AddNewItemIntent) => void;
+};
+
+export const AddItemColorField: React.FC<Props> = ({
+  colorName,
+  processIntent,
+}) => {
+  const defaultColor =
+    colorListWithNames.find(c => c.name === colorName) ?? colorListWithNames[0];
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleSelect = (colorItem: ColorItem) => {
+    processIntent({
+      type: COLOR_CHANGED,
+      newColorName: colorItem.name,
+      newColor: colorItem.color,
+    });
+    setShowDropdown(false);
+  };
+
+  return (
+    <View style={styles.wrapper}>
+      <ColorPickerRow
+        selectedColor={defaultColor}
+        showDropdown={showDropdown}
+        setShowDropdown={setShowDropdown}
+        onColorSelected={handleSelect}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    marginTop: 16,
+    marginHorizontal: 24,
+  },
+});
