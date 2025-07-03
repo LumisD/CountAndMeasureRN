297bca77fd6c23856e3a3c5186c11c4f733de6df
alexSimch <alexey@xealenergy.com>
2025-07-03 16:46:24 -0400

    Create ListOfNewItems

---
+++ src/screens/addnewitem/components/ListOfNewItems.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  FlatList,
+  Text,
+  View,
+  Pressable,
+  StyleSheet,
+  GestureResponderEvent,
+} from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import {AddNewItemIntent} from "../AddNewItemIntent";
+import {ChipboardUI} from "../models/ChipboardUI";
+import {PurpleGrey80} from "../../../theme/colors";
+import {ColorRectangle} from "../../common/components/ColorRectangle";
+
+interface Props {
+  hasColor: boolean;
+  chipboards: ChipboardUI[];
+  processIntent: (intent: AddNewItemIntent) => void;
+}
+
+export const ListOfNewItems: React.FC<Props> = ({
+  hasColor,
+  chipboards,
+  processIntent,
+}) => {
+  return (
+    <FlatList
+      data={chipboards}
+      keyExtractor={item => item.id.toString()}
+      ItemSeparatorComponent={() => (
+        <View style={[styles.separator, {backgroundColor: PurpleGrey80}]} />
+      )}
+      renderItem={({item}) => (
+        <Pressable
+          onPress={() =>
+            processIntent({type: "AskEditChipboard", chipboard: item})
+          }>
+          <View style={styles.row}>
+            <View style={styles.chipboardRow}>
+              <Text style={styles.chipboardText}>{item.chipboardAsString}</Text>
+              {hasColor && <ColorRectangle color={item.color} />}
+            </View>
+            <Pressable
+              onPress={() =>
+                processIntent({type: "AskDeleteChipboard", chipboard: item})
+              }>
+              <Icon name="close" size={24} style={styles.icon} />
+            </Pressable>
+          </View>
+        </Pressable>
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  chipboardRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+  },
+  chipboardText: {
+    flex: 1,
+    fontSize: 16,
+  },
+  icon: {
+    transform: [{scale: 1.3}],
+  },
+  separator: {
+    height: 4,
+  },
+});
