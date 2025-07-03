6085ab1b939790789369bd87d6b529f3d690fd26
alexSimch <alexey@xealenergy.com>
2025-07-03 15:02:10 -0400

    Create ExpandHideFieldProps

---
+++ src/screens/common/components/ExpandHideFieldInternal.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import {View, Pressable, StyleSheet, Animated} from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+interface ExpandHideFieldProps<T> {
+  isAreaOpen: boolean;
+  isRestoreIconShown: boolean;
+  processIntent: (intent: T) => void;
+  intentFactory: () => T;
+  onShareClick: (intent: T) => void;
+  shareFactory: () => T;
+  onDeleteClick: (intent: T) => void;
+  deleteFactory: () => T;
+}
+
+export function ExpandHideFieldInternal<T>({
+  isAreaOpen,
+  isRestoreIconShown,
+  processIntent,
+  intentFactory,
+  onShareClick,
+  shareFactory,
+  onDeleteClick,
+  deleteFactory,
+}: ExpandHideFieldProps<T>) {
+  const rotation = React.useRef(new Animated.Value(isAreaOpen ? 0 : 1)).current;
+
+  React.useEffect(() => {
+    Animated.timing(rotation, {
+      toValue: isAreaOpen ? 0 : 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }, [isAreaOpen]);
+
+  const rotateInterpolate = rotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "180deg"],
+  });
+
+  return (
+    <View style={styles.row}>
+      <Pressable
+        onPress={() => onDeleteClick(deleteFactory())}
+        style={[styles.iconWrapper, {opacity: 0.35}]}>
+        <Icon
+          name={isRestoreIconShown ? "restore" : "delete"}
+          accessibilityLabel="Delete or Restore"
+          size={32}
+          color="black"
+        />
+      </Pressable>
+
+      <Pressable
+        onPress={() => processIntent(intentFactory())}
+        style={styles.iconWrapper}>
+        <Animated.View style={{transform: [{rotate: rotateInterpolate}]}}>
+          <Icon name="keyboard-arrow-up" size={32} color="black" />
+        </Animated.View>
+      </Pressable>
+
+      <Pressable
+        onPress={() => onShareClick(shareFactory())}
+        style={[styles.iconWrapper, {opacity: 0.35}]}>
+        <Icon name="share" size={32} color="black" />
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+  iconWrapper: {
+    width: 48,
+    height: 48,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
