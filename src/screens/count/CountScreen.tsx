b55fe276c351f21fbc61e3dee407372adc648db8
alexSimch <alexey@xealenergy.com>
2025-07-07 09:40:54 -0400

    Get navigation, props, store in CountScreen

--- src/screens/count/CountScreen.tsx
+++ src/screens/count/CountScreen.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import {StackScreenProps} from "@react-navigation/stack";
+import React, {useEffect, useMemo} from "react";
+import {View, Text} from "react-native";
+import {RootStackParamList} from "../../navigation/types";
+import {useRealm} from "../../data/db/RealmContext";
+import {provideMeasureAndCountRepository} from "../../data/db/dao/provideRepository";
+import {createCountStore} from "./store/CountStore";
+import {useStore} from "zustand";
+import {CLEANUP, START} from "./CountIntent";
 
-export default function CountScreen() {
+type Props = StackScreenProps<RootStackParamList, "Count">;
+
+export default function CountScreen({navigation, route}: Props) {
+  const {unionId} = route.params;
+
+  const realm = useRealm();
+  const repo = useMemo(() => provideMeasureAndCountRepository(realm), [realm]);
+  const store = useMemo(() => createCountStore(repo), [repo]);
+
+  const state = useStore(store, s => s.state);
+  const processIntent = useStore(store, s => s.processIntent);
+  const currentEffect = useStore(store, s => s.currentEffect);
+  const consumeEffect = useStore(store, s => s.consumeEffect);
+
+  useEffect(() => {
+    processIntent({type: START, unionId: unionId});
+    return () => {
+      processIntent({type: CLEANUP});
+    };
+  }, []);
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
       <Text>Count Screen</Text>
     </View>
   );
