d85a4a3473d2ec874c10526460cc4c20d5b17760
alexSimch <alexey@xealenergy.com>
2025-07-10 11:23:12 -0400

    Handle PRESS_TO_DELETE_OR_RESTORE_UNION in reducer

--- src/screens/count/CountReducer.ts
+++ src/screens/count/CountReducer.ts
@@ -1,5 +1,16 @@
-import {CountEffect, NAVIGATE_BACK} from "./CountEffect";
-import {CountIntent, TOGGLE_FIND_AREA_VISIBILITY, BACK} from "./CountIntent";
+import {
+  CountEffect,
+  NAVIGATE_BACK,
+  SHOW_DELETE_UNION_DIALOG,
+  SHOW_RESTORE_UNION_DIALOG,
+} from "./CountEffect";
+import {
+  CountIntent,
+  TOGGLE_FIND_AREA_VISIBILITY,
+  BACK,
+  PRESS_TO_SHARE_UNION,
+  PRESS_TO_DELETE_OR_RESTORE_UNION,
+} from "./CountIntent";
 import {CountState} from "./CountState";
 
 export const countReducer = (
@@ -21,6 +32,19 @@ export const countReducer = (
         newState: state,
         effect: {type: NAVIGATE_BACK},
       };
+
+    case PRESS_TO_DELETE_OR_RESTORE_UNION: {
+      const {unionOfChipboards} = state;
+
+      return {
+        newState: state,
+        effect: {
+          type: unionOfChipboards.isMarkedAsDeleted
+            ? SHOW_RESTORE_UNION_DIALOG
+            : SHOW_DELETE_UNION_DIALOG,
+        },
+      };
+    }
 
     default:
       console.warn("MaC countReducer Unhandled intent:", intent);
