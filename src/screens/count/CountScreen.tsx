97aa4e4ada092b3d8711e2f8c66f210dfd75a5d2
alexSimch <alexey@xealenergy.com>
2025-07-11 14:06:25 -0400

    Add handleCountffects in CountScreen

--- src/screens/count/CountScreen.tsx
+++ src/screens/count/CountScreen.tsx
@@ -1,5 +1,5 @@
 import {StackScreenProps} from "@react-navigation/stack";
-import React, {useEffect, useMemo} from "react";
+import React, {useEffect, useMemo, useState} from "react";
 import {View, Text} from "react-native";
 import {RootStackParamList} from "../../navigation/types";
 import {useRealm} from "../../data/db/RealmContext";
@@ -7,6 +7,7 @@ import {provideMeasureAndCountRepository} from "../../data/db/dao/provideReposit
 import {createCountStore} from "./store/CountStore";
 import {useStore} from "zustand";
 import {CLEANUP, START} from "./CountIntent";
+import {handleCountffects} from "../addnewitem/handlers/handleCountEffect";
 
 type Props = StackScreenProps<RootStackParamList, "Count">;
 
@@ -22,12 +23,32 @@ export default function CountScreen({navigation, route}: Props) {
   const currentEffect = useStore(store, s => s.currentEffect);
   const consumeEffect = useStore(store, s => s.consumeEffect);
 
+  const [snackbarVisible, setSnackbarVisible] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+
+  const [shouldFlash, setShouldFlash] = useState(false);
+
   useEffect(() => {
     processIntent({type: START, unionId: unionId});
     return () => {
       processIntent({type: CLEANUP});
     };
   }, []);
+
+  useEffect(() => {
+    if (!currentEffect) return;
+
+    handleCountffects(
+      currentEffect,
+      setSnackbarMessage,
+      setSnackbarVisible,
+      setShouldFlash,
+      navigation,
+      processIntent,
+    );
+
+    consumeEffect();
+  }, [currentEffect, consumeEffect]);
 
   return (
     <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
