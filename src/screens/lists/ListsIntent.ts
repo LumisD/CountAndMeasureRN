d66136c844463e64bd8a1a16468b3ef969f0d7bc
alexSimch <alexey@xealenergy.com>
2025-07-12 04:03:35 -0400

    Add START and CLEANUP intents

--- src/screens/lists/ListsIntent.ts
+++ src/screens/lists/ListsIntent.ts
@@ -1,8 +1,12 @@
 import {UnionOfChipboardsUI} from "../models/UnionOfChipboardsUI";
 
+// Handled in handleAddNewItemIntent
+export const START = "GetUnions" as const;
+export const CLEANUP = "Cleanup" as const;
+// Handled in reducer
 export const PRESS_ON_ITEM_IN_LIST = "PressOnItemList";
 
-export type ListsIntent = {
-  type: typeof PRESS_ON_ITEM_IN_LIST;
-  union: UnionOfChipboardsUI;
-};
+export type ListsIntent =
+  | {type: typeof START}
+  | {type: typeof CLEANUP}
+  | {type: typeof PRESS_ON_ITEM_IN_LIST; union: UnionOfChipboardsUI};
