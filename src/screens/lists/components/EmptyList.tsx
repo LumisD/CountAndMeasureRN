e213c24ab474bc52bde15ea5125919bdef9dceaa
alexSimch <alexey@xealenergy.com>
2025-07-12 16:36:27 -0400

    Adjusments in EmptyList

--- src/screens/lists/components/EmptyList.tsx
+++ src/screens/lists/components/EmptyList.tsx
@@ -1,6 +1,7 @@
 import {t} from "i18next";
 import {StyleSheet, Text, View} from "react-native";
 import {Typography} from "../../../theme/typography";
+import {MainBg} from "../../../theme/colors";
 
 export function EmptyList() {
   return (
@@ -15,11 +16,13 @@ export function EmptyList() {
 const styles = StyleSheet.create({
   emptyBox: {
     flex: 1,
+    backgroundColor: MainBg,
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 16,
   },
   emptyText: {
     ...Typography.bodyLarge,
+    textAlign: "center",
   },
 });
